b83ea2e631d42d5507b0ce8728bc35af5c8f8e55
Michael Lei <michaelleie0ca@gmail.com>
2023-08-05 14:50:36 -0700

    Finish CRUD courses

--- services/apiCourse.ts
+++ services/apiCourse.ts
@@ -5,16 +5,12 @@ export type PartialCourse = Pick<Course, "name" | "description">;
 
 export async function createCourse(course: PartialCourse) {
   const { error } = await supabase.from("courses").insert([course]);
-  if (error) {
-    throw error;
-  }
+  if (error) throw error;
 }
 
 export async function getCourses() {
   const { data, error } = await supabase.from("course").select("*");
-  if (error) {
-    throw error;
-  }
+  if (error) throw error;
   return data;
 }
 
@@ -24,22 +20,16 @@ export async function getCourse(id: string) {
     .select("*")
     .match({ id })
     .single();
-  if (error) {
-    throw error;
-  }
+  if (error) throw error;
   return data;
 }
 
 export async function updateCourse(id: string, course: PartialCourse) {
   const { error } = await supabase.from("course").update(course).match({ id });
-  if (error) {
-    throw error;
-  }
+  if (error) throw error;
 }
 
 export async function deleteCourse(id: string) {
   const { error } = await supabase.from("course").delete().match({ id });
-  if (error) {
-    throw error;
-  }
+  if (error) throw error;
 }
