39c1d665d904806d0a1460b053f275f339ec0ac3
Michael Lei <michaelleie0ca@gmail.com>
2023-08-05 00:47:57 -0700

    Add dark mode toggle

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import Nav from "@/components/Nav";
+import { ModeToggle } from "@/components/mode-toggle";
 import { ThemeProvider } from "@/components/theme-provider";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
@@ -18,8 +19,11 @@ function Layout({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       <div className={`${inter.className}`}>
-        <div className="p-5">
+        <div className="flex items-center p-5">
           <Nav />
+          <div className="ml-auto">
+            <ModeToggle />
+          </div>
         </div>
         <main className="p-5 xl:p-10">{children}</main>
       </div>
