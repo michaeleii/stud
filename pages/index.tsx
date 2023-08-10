c592210a3ab887b3ce852cabdaf948fef16d0afa
Michael Lei <michaelleie0ca@gmail.com>
2023-08-10 15:37:37 -0700

    Finish dashboard layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
+import Dashboard from "@/components/Dashboard";
 import Greeting from "@/components/Greeting";
 
 export default function Home() {
   return (
     <>
       <Greeting />
+      <Dashboard />
     </>
   );
 }
