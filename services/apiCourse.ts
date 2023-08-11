fd45d7a7b6f450e5b9025017d21bfabfce455cea
Michael Lei <michaelleie0ca@gmail.com>
2023-08-10 17:29:19 -0700

    Add task count

--- services/apiCourse.ts
+++ services/apiCourse.ts
@@ -1,6 +1,7 @@
 import supabase from "./supabase";
 
-export type Course = Awaited<ReturnType<typeof getCourses>>[0];
+export type CourseWithCount = Awaited<ReturnType<typeof getCourses>>[0];
+export type Course = Omit<CourseWithCount, "totalCount" | "completedCount">;
 export type PartialCourse = Omit<Course, "id" | "created_at">;
 
 export async function createCourse(course: PartialCourse) {
@@ -11,7 +12,24 @@ export async function createCourse(course: PartialCourse) {
 export async function getCourses() {
   const { data, error } = await supabase.from("course").select("*");
   if (error) throw error;
-  return data;
+
+  const courses = await Promise.all(
+    data.map(async (course) => {
+      const { count: totalCount, error } = await supabase
+        .from("task")
+        .select("*", { count: "exact" })
+        .match({ course_id: course.id });
+      const { count: completedCount, error: completedTasksError } =
+        await supabase
+          .from("task")
+          .select("*", { count: "exact" })
+          .match({ course_id: course.id, is_completed: true });
+      if (error) throw error;
+      if (completedTasksError) throw completedTasksError;
+      return { ...course, totalCount, completedCount };
+    })
+  );
+  return courses;
 }
 
 export async function getCourse(id: Course["id"]) {
@@ -21,6 +39,7 @@ export async function getCourse(id: Course["id"]) {
     .match({ id })
     .single();
   if (error) throw error;
+
   return data;
 }
 
