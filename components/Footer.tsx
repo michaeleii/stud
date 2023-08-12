290b0a7e95b68636861e5f966527a54c25c3f218
Michael Lei <michaelleie0ca@gmail.com>
2023-08-12 15:06:57 -0700

    Add footer links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,21 @@ function Footer() {
   return (
     <footer className="mt-10 flex flex-col items-center justify-center gap-4 tracking-tight md:h-24 md:flex-row">
       <div>
-        Built by Michael and Scott. Built using{" "}
+        Built by{" "}
+        <a
+          href="https://github.com/michaeleii"
+          className="font-medium underline underline-offset-4"
+        >
+          Michael
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://github.com/scottchen98"
+          className="font-medium underline underline-offset-4"
+        >
+          Scott
+        </a>
+        . Built using{" "}
         <a
           href="https://nextjs.org/"
           className="font-medium underline underline-offset-4"
