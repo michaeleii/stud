99edfea3aa62f526a44090d3fbae67150c16a20f
Michael Lei <michaelleie0ca@gmail.com>
2023-08-12 22:36:37 -0700

    Add error

--- hooks/course/useCourse.ts
+++ hooks/course/useCourse.ts
@@ -1,11 +1,19 @@
 import { getCourse } from "@/services/apiCourse";
 import { useQuery } from "@tanstack/react-query";
+import { useRouter } from "next/router";
 
 export function useCourse(id: number) {
-  const { data: course, isLoading } = useQuery({
+  const router = useRouter();
+  const {
+    data: course,
+    isLoading,
+    isError,
+  } = useQuery({
     queryKey: ["course"],
     queryFn: () => getCourse(id),
     enabled: !!id,
   });
+  if (isError) router.push("/404");
+
   return { course, isLoading };
 }
