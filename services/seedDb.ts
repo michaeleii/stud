087394919d2754e96f6fecb961493963c5daaed9
Michael Lei <michaelleie0ca@gmail.com>
2023-08-12 22:46:28 -0700

    Remove seed db

--- services/seedDb.ts
+++ services/seedDb.ts
@@ -1,15 +1,15 @@
-import { courses } from "@/data/courses";
-import supabase from "./supabase";
+// import { courses } from "@/data/courses";
+// import supabase from "./supabase";
 
-export async function seedDB() {
-  const randomCourses = courses.map((course) => {
-    return {
-      name: course.name,
-      description: course.description,
-      color: course.color,
-      schedule: course.schedule,
-    };
-  });
-  const { error } = await supabase.from("course").insert(randomCourses);
-  if (error) throw error;
-}
+// export async function seedDB() {
+//   const randomCourses = courses.map((course) => {
+//     return {
+//       name: course.name,
+//       description: course.description,
+//       color: course.color,
+//       schedule: course.schedule,
+//     };
+//   });
+//   const { error } = await supabase.from("course").insert(randomCourses);
+//   if (error) throw error;
+// }
